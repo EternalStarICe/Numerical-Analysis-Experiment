d533a6d48d26d4ad7f618732af60fd39bd76c7aa
glaucous <975189452@qq.com>
2020-05-01 03:44:14 +0800

    add

---
+++ ODE/RungeKutta4degree.m
@@ -0,0 +1,18 @@
+% dy/dx = 2xy
+% y(0) = 2
+clear all; clc;
+x0 = 0;
+y0 = 2;
+h = 0.01;
+f = @(x, y)(2 * x * y);
+for steps = 1:100
+    k1 = f(x0, y0);
+    k2 = f(x0 + h/2, y0 + h*k1/2);
+    k3 = f(x0 + h/2, y0 + h*k2/2);
+    k4 = f(x0 + h, y0 + h*k3);
+    x1 = x0 + h;
+    y1 = y0 + h * (k1 + 2*k2 + 2*k3 + k4) / 6;
+    x0 = x1;
+    y0 = y1;
+    fprintf("x = %d, y = %d\n", x0, y0);
+end
---
+++ ODE/modifiedEulerMethod.m
@@ -0,0 +1,16 @@
+% dy/dx = 2xy
+% y(0) = 2
+clear all; clc;
+x0 = 0;
+y0 = 2;
+h = 0.01;
+f = @(x, y)(2 * x * y);
+g = @(x, y)(y + h / 2 * (f(x, y) + f(x0, y0)));
+for step = 1:100
+    y1 = y0 + h * f(x0, y0);
+    x1 = x0 + h;
+    y1 = y0 + h / 2 * (f(x0, y0) + f(x1, y1));
+    fprintf("x = %d, y = %d\n", x1, y1);
+    x0 = x1;
+    y0 = y1;
+end
